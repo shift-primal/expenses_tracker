adf1c57e0e2246c7e9b6789003360b23a5f4b092
Kasper Haugestøl <kasper@haugestol.com>
2026-02-17 21:13:56 +0100

    litt endpoints

--- backend/ExpensesApi/Endpoints/CategoryEndpoints.cs
+++ backend/ExpensesApi/Endpoints/CategoryEndpoints.cs
@@ -2,5 +2,62 @@ using Microsoft.EntityFrameworkCore;
 
 public static class CategoryEndpoints
 {
-    public static void Map(WebApplication app) { }
+    public static void Map(WebApplication app)
+    {
+        app.MapGet("/categories", async (ExpensesDb db) => await db.Categories.ToListAsync());
+
+        app.MapGet(
+            "/categories/{id}",
+            async (int id, ExpensesDb db) =>
+                await db.Categories.FindAsync(id) is Category c ? Results.Ok(c) : Results.NotFound()
+        );
+
+        app.MapPost(
+            "/categories",
+            async (Category c, ExpensesDb db) =>
+            {
+                db.Categories.Add(c);
+                await db.SaveChangesAsync();
+
+                return Results.Created($"/categories/{c.Id}", c);
+            }
+        );
+
+        app.MapPut(
+            "/categories/{id}",
+            async (int id, Category inputC, ExpensesDb db) =>
+            {
+                var c = await db.Categories.FindAsync(id);
+
+                if (c is null)
+                    return Results.NotFound();
+
+                c.Name = inputC.Name;
+                c.IsDefault = inputC.IsDefault;
+                c.Rules = inputC.Rules;
+
+                await db.SaveChangesAsync();
+
+                return Results.NoContent();
+            }
+        );
+
+        app.MapDelete(
+            "/categories/{id}",
+            async (int id, ExpensesDb db) =>
+            {
+                // TODO
+                // Gjøre så transaksjoner med kategorien flyttes til "annet" først
+
+                if (await db.Categories.FindAsync(id) is Category c)
+                {
+                    db.Categories.Remove(c);
+                    await db.SaveChangesAsync();
+                    return Results.NoContent();
+                }
+
+                return Results.NotFound();
+            }
+        );
+    }
 }
--- backend/ExpensesApi/Endpoints/TransactionEndpoints.cs
+++ backend/ExpensesApi/Endpoints/TransactionEndpoints.cs
@@ -6,14 +6,48 @@ public static class TransactionEndpoints
     {
         app.MapGet("/transactions", async (ExpensesDb db) => await db.Transactions.ToListAsync());
 
-        app.MapPost(
-            "/transactions",
-            async (Transaction t, ExpensesDb db) =>
+        app.MapGet(
+            "/transactions/{id}",
+            async (Guid id, ExpensesDb db) =>
+                await db.Transactions.FindAsync(id) is Transaction t
+                    ? Results.Ok(t)
+                    : Results.NotFound()
+        );
+
+        app.MapPut(
+            "/transactions/{id}",
+            async (Guid id, Transaction inputT, ExpensesDb db) =>
             {
-                db.Transactions.Add(t);
+                var t = await db.Transactions.FindAsync(id);
+
+                if (t is null)
+                    return Results.NotFound();
+
+                t.AccountSource = inputT.AccountSource;
+                t.Amount = inputT.Amount;
+                t.Category = inputT.Category;
+                t.CreatedAt = inputT.CreatedAt;
+                t.Date = inputT.Date;
+                t.Description = inputT.Description;
+
                 await db.SaveChangesAsync();
 
-                return Results.Created($"/transactions/{t.Id}", t);
+                return Results.NoContent();
+            }
+        );
+
+        app.MapDelete(
+            "/transactions/{id}",
+            async (Guid id, ExpensesDb db) =>
+            {
+                if (await db.Transactions.FindAsync(id) is Transaction t)
+                {
+                    db.Transactions.Remove(t);
+                    await db.SaveChangesAsync();
+                    return Results.NoContent();
+                }
+
+                return Results.NotFound();
             }
         );
     }
